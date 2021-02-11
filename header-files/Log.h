b84675f51cf5dc4f8324976337af4e0bb8896603
lyx0 <stefan.parfuss@protonmail.ch>
2021-02-11 14:44:46 +0100

    xd

---
+++ header-files/Log.h
@@ -0,0 +1,5 @@
+
+void Log(const char* message)
+{
+    std::cout << message << std::endl;
+};
---
+++ header-files/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Log.h"
+
+
+int main()
+{
+    Log("Hello");
+    std::cin.get();
+};
