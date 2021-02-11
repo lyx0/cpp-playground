62a3d59ea4663f49cabccaf3b9e2856bd5e108d6
lyx0 <stefan.parfuss@protonmail.ch>
2021-02-11 15:19:37 +0100

    xd

---
+++ how-to-debug/Log.h
@@ -0,0 +1,4 @@
+void Log(const char* message)
+{
+    std::cout << message << std::endl;
+};
---
+++ how-to-debug/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Log.h"
+
+int main() 
+{
+    Log("Hello");
+    std::cin.get();
+};
